2067d6ba7883728c02c260dab3783db75f71e406
divyanshu1234 <divyanshu.pundir@gmail.com>
2019-03-26 11:38:52 +0530

    refactor: Code formatting and spelling fixes in strings

--- timey/timey.py
+++ timey/timey.py
@@ -1,13 +1,18 @@
-import click
-import os,sys, pickle
-from datetime import datetime, timedelta
 import logging
+import os
+import pickle
+from datetime import datetime, timedelta
 from enum import Enum
+
+import click
+
+
 # logging.basicConfig(level=logging.DEBUG)
 
-class status(Enum):
+class Status(Enum):
     STARTED = '1'
     STOPPED = '2'
+
 
 def load_timekeeper():
     if os.path.isfile(file_path()):
@@ -19,12 +24,13 @@ def load_timekeeper():
         timekeeper = {}
     return timekeeper
 
+
 def file_path():
     today = datetime.today().strftime("%Y-%d-%m")
-    path = os.path.join(os.path.dirname(__file__),'db') 
+    path = os.path.join(os.path.dirname(__file__), 'db')
     if not os.path.isdir(path):
         os.makedirs(path)
-    path = os.path.join(path,today + '.pkl')
+    path = os.path.join(path, today + '.pkl')
     return path
 
 
@@ -33,7 +39,8 @@ def file_path():
 def main(ctx):
     logging.debug('In main')
     timekeeper = load_timekeeper()
-    ctx.obj = timekeeper    
+    ctx.obj = timekeeper
+
 
 @main.command()
 @click.argument('tag', type=str)
@@ -48,20 +55,22 @@ def start(timekeeper, tag):
     timekeeper = _start(timekeeper, tag)
     save_timekeeper(timekeeper)
 
+
 def _start(timekeeper, tag):
     if tag in timekeeper.keys():
-        if timekeeper[tag][0] == status.STARTED:
+        if timekeeper[tag][0] == Status.STARTED:
             print(f"Timer for {tag} has been running since {format_time(timekeeper[tag][1][-1])}")
         else:
             print(f"New session for {tag}.\nLast run ran for {last2runs(timekeeper,tag)}.\nCurent time is {timekeeper[tag][1][-1].strftime('%H:%M')}.")
             timekeeper[tag][1].append(datetime.today())
-            timekeeper[tag][0] = status.STARTED
+            timekeeper[tag][0] = Status.STARTED
             return timekeeper
     else:
-        timekeeper[tag] = [status.STARTED, [datetime.today()]]
+        timekeeper[tag] = [Status.STARTED, [datetime.today()]]
         print(f"New session for new tag: {tag}.")
-        print(f"Curent time is {timekeeper[tag][1][-1].strftime('%H:%M')}.")
+        print(f"Current time is {timekeeper[tag][1][-1].strftime('%H:%M')}.")
         return timekeeper
+
 
 @main.command()
 @click.argument('tag', type=str)
@@ -73,68 +82,73 @@ def tick(timekeeper, tag):
         tag {string} -- tag for which to get the time for 
     """
     logging.debug("In main")
-    if timekeeper[tag][0] == status.STOPPED:
+    if timekeeper[tag][0] == Status.STOPPED:
         print(f"Timer for {tag} is not running. Last run ran for {last2runs(timekeeper,tag)}.")
-    elif timekeeper[tag][0] == status.STARTED:
+    elif timekeeper[tag][0] == Status.STARTED:
         delta = datetime.today() - timekeeper[tag][1][-1]
         print(f"{format_delta(delta)} elapsed.\nStarted at {format_time(timekeeper[tag][1][-1])}.")
+
 
 @main.command()
 @click.argument('tag', type=str, default='')
 @click.pass_obj
 def stop(timekeeper, tag):
-    '''Stops the timer for a tag if provided or stops all running tags
-    
+    """Stops the timer for a tag if provided or stops all running tags
+
     Arguments:
         tag {string} -- tag to stop
-    '''
+    """
 
     if tag == '':
         for _tag in timekeeper.keys():
-            if timekeeper[_tag][0] != status.STOPPED:
+            if timekeeper[_tag][0] != Status.STOPPED:
                 timekeeper = _stop(timekeeper, _tag)
     else:
         timekeeper = _stop(timekeeper, tag)
     save_timekeeper(timekeeper)
 
+
 def _stop(timekeeper, tag):
-    if timekeeper[tag][0] == status.STOPPED:
+    if timekeeper[tag][0] == Status.STOPPED:
         print(f"Timer for {tag} was already stopped at {format_time(timekeeper[tag][1][-1])}")
     else:
         timekeeper[tag][1].append(datetime.today())
-        timekeeper[tag][0] = status.STOPPED
-        print(f"Sessiong for {tag} stopped. Last run ran for {last2runs(timekeeper,tag)}.")
+        timekeeper[tag][0] = Status.STOPPED
+        print(f"Session for {tag} stopped. Last run ran for {last2runs(timekeeper,tag)}.")
         return timekeeper
+
 
 @main.command()
 @click.argument('tag', type=str)
 @click.pass_obj
 def switch(timekeeper, tag):
-    '''Stops timer for currently running tags and starts a new session for tag 
-    
+    """Stops timer for currently running tags and starts a new session for tag
+
     Arguments:
         tag {string} -- tag to switch timer to
-    '''
+    """
     for _tag in timekeeper.keys():
-        if timekeeper[_tag][0] != status.STOPPED:
+        if timekeeper[_tag][0] != Status.STOPPED:
             timekeeper = _stop(timekeeper, _tag)
-    timekeeper = _start(timekeeper,tag)
+    timekeeper = _start(timekeeper, tag)
     save_timekeeper(timekeeper)
+
 
 def save_timekeeper(timekeeper, test=False):
     with open(file_path(), 'wb') as file:
-        pickle.dump(timekeeper,file)
+        pickle.dump(timekeeper, file)
     logging.debug("File saved")
+
 
 @main.command()
 @click.argument('tag', type=str, default='')
 @click.pass_obj
 def summarise(timekeeper, tag):
-    '''Summarises session history for tag if provided or for all
-    
+    """Summarises session history for tag if provided or for all
+
     Arguments:
         tag {string} -- tag to summarise sessions for
-    '''
+    """
 
     if tag == '':
         for _tag in timekeeper.keys():
@@ -143,27 +157,31 @@ def summarise(timekeeper, tag):
     else:
         _summarise(timekeeper, tag)
 
+
 def last2runs(timekeeper, tag):
     return format_delta(timekeeper[tag][1][-1] - timekeeper[tag][1][-2])
 
+
 def format_time(time):
     return time.strftime('%H:%M')
+
 
 def format_delta(delta):
     seconds = delta.total_seconds()
     return f"{int(seconds//3600)}h {int(seconds%3600//60)}m {int(seconds%60)}s"
 
+
 def _summarise(timekeeper, tag):
     print(f"Summary for {tag}")
     total_time = timedelta()
     timelist = timekeeper[tag][1]
-    for i in range(len(timelist)//2):
-        delta = timelist[2*i+1]-timelist[2*i]
+    for i in range(len(timelist) // 2):
+        delta = timelist[2 * i + 1] - timelist[2 * i]
         print(f"{i+1}. {format_time(timelist[2*i])} {format_delta(delta)}")
         total_time += delta
 
-    if timekeeper[tag][0] == status.STARTED:
-        delta = datetime.today()-timelist[-1]
+    if timekeeper[tag][0] == Status.STARTED:
+        delta = datetime.today() - timelist[-1]
         total_time += delta
-        print(f"{len(timelist)//2+1}. {format_time(timelist[-1])} {format_delta(delta)} - running") 
+        print(f"{len(timelist)//2+1}. {format_time(timelist[-1])} {format_delta(delta)} - running")
     print(f"Total time spent - {format_delta(total_time)} ")
